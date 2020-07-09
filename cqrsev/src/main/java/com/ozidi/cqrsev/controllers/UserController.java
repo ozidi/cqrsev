4a7168fc05e240c52a7a6f61002c2f9cbb07dad7
ozidi <ozidi@OZidi-TN>
2020-07-09 12:44:09 +0100

    Add Controller & Application Starter

---
+++ cqrsev/src/main/java/com/ozidi/cqrsev/controllers/UserController.java
@@ -0,0 +1,70 @@
+package com.ozidi.cqrsev.controllers;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ozidi.cqrsev.domains.User;
+import com.ozidi.cqrsev.repository.UserRepository;
+
+@Controller
+public class UserController {
+
+	private UserRepository userRepository;
+
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@GetMapping("/signup")
+	public String showSignUpForm(User user) {
+		return "add-user";
+
+	}
+
+	@PostMapping("/adduser")
+	public String addUser(@Valid User user, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "add-user";
+		}
+
+		userRepository.save(user);
+		model.addAttribute("users", userRepository.findAll());
+		return "redirect:/index";
+
+	}
+
+	@GetMapping("/edit/{id}")
+	public String showUpdateForm(@PathVariable("id") long id, Model model) {
+		Optional<User> userFound = userRepository.findById(id);
+		model.addAttribute("user", userFound.get());
+		return "update-user";
+	}
+
+	@PostMapping("/update/{id}")
+	public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			user.setUserId(id);
+			return "update-user";
+		}
+		userRepository.save(user);
+		model.addAttribute("users", userRepository.findAll());
+		return "redirect:/index";
+
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+		Optional<User> userFound = userRepository.findById(id);
+		userRepository.delete(userFound.get());
+		model.addAttribute("users", userRepository.findAll());
+		return "index";
+	}
+}
